476d5dc80562329d49f2a95e7fe2fec03c86bd40
Taehyun Kim <k1m743hyun@icloud.com>
2025-01-15 15:31:38 +0900

    Connect the PostgreSQL to the Spring Batch

--- src/main/java/com/k1m743hyun/batchservice/batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/k1m743hyun/batchservice/batch/job/SimpleJobConfiguration.java
@@ -1,7 +1,9 @@
 package com.k1m743hyun.batchservice.batch.job;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -13,25 +15,45 @@ import org.springframework.transaction.PlatformTransactionManager;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 @Configuration
 public class SimpleJobConfiguration {
 
+    private final PlatformTransactionManager platformTransactionManager;
+    private final JobRepository jobRepository;
+
     @Bean
-    public Job simpleJob1(JobRepository jobRepository, Step simpleStep1) {
+    public Job simpleJob1(Step simpleStep1, Step simpleStep2) {
         return new JobBuilder("simpleJob", jobRepository)
-            .start(simpleStep1)
-            .build();
+                .start(simpleStep1)
+                .next(simpleStep2)
+                .build();
     }
+
+    @JobScope
     @Bean
-    public Step simpleStep1(JobRepository jobRepository, Tasklet testTasklet, PlatformTransactionManager platformTransactionManager){
+    public Step simpleStep1(Tasklet testTasklet) {
         return new StepBuilder("simpleStep1", jobRepository)
-            .tasklet(testTasklet, platformTransactionManager).build();
+                .tasklet(testTasklet, platformTransactionManager)
+                .build();
     }
+
     @Bean
-    public Tasklet testTasklet(){
-        return ((contribution, chunkContext) -> {
+    public Tasklet testTasklet() {
+        return (contribution, chunkContext) -> {
             log.info(">>>>> This is Step1");
             return RepeatStatus.FINISHED;
-        });
+        };
+    }
+
+    @JobScope
+    @Bean
+    public Step simpleStep2() {
+        return new StepBuilder("simpleStep2", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
+                    return RepeatStatus.FINISHED;
+                }, platformTransactionManager)
+                .build();
     }
 }
